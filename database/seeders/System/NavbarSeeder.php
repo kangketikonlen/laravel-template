26d209c139f0ea9ef23d52342251e941e51af7e0
Gilang Pratama <pratamapriadi96@gmail.com>
2023-08-16 01:57:52 +0700

    Update navbar seeder.

--- database/seeders/System/NavbarSeeder.php
+++ database/seeders/System/NavbarSeeder.php
@@ -33,7 +33,7 @@ class NavbarSeeder extends Seeder
             'name' => 'Setting',
             'icon' => 'settings-1',
             'url' => '#',
-            'roles' => "general",
+            'roles' => "",
             'type' => 'dropdown'
         ]);
 
@@ -41,7 +41,7 @@ class NavbarSeeder extends Seeder
             'name' => 'Administrasi',
             'icon' => 'edit-square',
             'url' => '#',
-            'roles' => "general",
+            'roles' => "",
             'type' => 'dropdown'
         ]);
 
@@ -49,7 +49,7 @@ class NavbarSeeder extends Seeder
             'name' => 'Laporan',
             'icon' => 'book',
             'url' => '#',
-            'roles' => "general",
+            'roles' => "",
             'type' => 'dropdown'
         ]);
     }
