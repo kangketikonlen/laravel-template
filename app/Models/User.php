3de0a075af09ac652e9a6ad256df36782d80626e
Gilang Pratama <pratamapriadi96@gmail.com>
2023-08-16 00:34:14 +0700

    Fix wrong functions.

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    public function users()
+    public function roles()
     {
         return $this->belongsTo(Role::class, 'role_id');
     }
