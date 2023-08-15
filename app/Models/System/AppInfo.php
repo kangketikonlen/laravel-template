88879e9b864c8c0c48c793b771c03ad71864cf3f
Gilang Pratama <pratamapriadi96@gmail.com>
2023-08-16 01:54:54 +0700

    Add log on app info.

--- app/Models/System/AppInfo.php
+++ app/Models/System/AppInfo.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class AppInfo extends Model
 {
     use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->createdAt = $model->freshTimestamp();
+            $model->createdBy = auth()->check() ? auth()->user()->name : "System";
+        });
+
+        static::updating(function ($model) {
+            $model->updatedAt = $model->freshTimestamp();
+            $model->updatedBy = auth()->check() ? auth()->user()->name : "System";
+        });
+    }
 }
