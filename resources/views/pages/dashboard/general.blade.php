a3572c0dcf87f7a74079be69b0558d1c2671657c
Gilang Pratama <pratamapriadi96@gmail.com>
2023-08-16 00:56:03 +0700

    Add general blade view.

---
+++ resources/views/pages/dashboard/general.blade.php
@@ -0,0 +1,30 @@
+@php
+    $html_tag_data = [];
+    $title = 'General Setting Dashboard';
+    $description = 'Dashboard';
+    $breadcrumbs = ['/' => 'Home'];
+@endphp
+@extends('layout-private', ['html_tag_data' => $html_tag_data, 'title' => $title, 'description' => $description])
+
+@section('css')
+@endsection
+
+@section('js_vendor')
+@endsection
+
+@section('js_page')
+@endsection
+
+@section('content')
+    <div class="container">
+        @include('_layout/breadcrumb')
+        <div class="row justify-content-center text-center">
+            <div class="col-12">
+                <h4 class="fw-bold">Hi, {{ session()->get('name') }}</h4>
+                <p class="m-0">
+                    Selamat datang di dashboard {{ session()->get('role_description') }}.
+                </p>
+            </div>
+        </div>
+    </div>
+@endsection
