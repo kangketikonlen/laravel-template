24179b5c161e7ffbe472d327e5aa58578f40b505
Gilang Pratama <pratamapriadi96@gmail.com>
2023-08-15 22:51:34 +0700

    Add webpack.mix.js & add resources.

---
+++ webpack.mix.js
@@ -0,0 +1,23 @@
+const mix = require("laravel-mix");
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+*/
+
+mix.options({
+    processCssUrls: false,
+});
+
+mix.copy("resources/favicon.ico", "public/favicon.ico");
+mix.copy("resources/css", "public/css");
+mix.copy("resources/font", "public/font");
+mix.copy("resources/icon", "public/icon");
+mix.copy("resources/img", "public/img");
+mix.copy("resources/js", "public/js");
+mix.copy("resources/plugin", "public/plugin");
+
+if (mix.inProduction()) {
+    mix.version();
+}
